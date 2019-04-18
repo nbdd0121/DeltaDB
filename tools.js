b76ae194d437a0ca6464d2a004ed123b53b3b484
Gary Guo <gary@garyguo.net>
2019-04-18 01:11:09 +0100

    Add validation tool

--- tools.js
+++ tools.js
@@ -1,9 +1,10 @@
+const { promisify } = require('util');
 
 /**
  * Copy one BlobDatabase into another one. Databases can have different compression settings.
  *
- * @param {BlobDatabase} oldDb 
- * @param {BlobDatabase} newDb 
+ * @param {BlobDatabase} oldDb
+ * @param {BlobDatabase} newDb
  */
 async function copy(oldDb, newDb) {
     let iter = oldDb._db.iterator();
@@ -15,7 +16,7 @@ async function copy(oldDb, newDb) {
     let bytes = 0;
     try {
         let k, v;
-        while ([k, v] = await next()) {
+        while ([k, v] = await next(), k) {
             let blob = await oldDb._rawToBlob(k, v);
             await newDb._blobPut(blob);
             bytes += v.length;
@@ -26,3 +27,30 @@ async function copy(oldDb, newDb) {
     }
 }
 exports.copy = copy;
+
+/**
+ * Call `get()` on all blobs.  If db is opened with `{ verify: true }`, this can be used to detect
+ * potential corruptions.
+ *
+ * @param {BlobDatabase} db
+ */
+async function validate(db) {
+    let iter = db._db.iterator();
+    let next = () => new Promise((resolve, reject) => iter.next((err, key) => {
+        if (err) return reject(err);
+        else resolve(key);
+    }));
+    console.log();
+    let entries = 0;
+    try {
+        let k;
+        while (k = await next()) {
+            await db.get(k);
+            entries++;
+            console.log(`\x1b[1A\r\x1b[0KProcessed ${entries} entries`);
+        }
+    } finally {
+        await promisify(cb => iter.end(cb))();
+    }
+}
+exports.validate = validate;
